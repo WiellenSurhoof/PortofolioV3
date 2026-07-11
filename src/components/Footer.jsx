9b1e59982fb090408e5e4cc0e25322e5298cc8a3
Wiel <wildanhakim164@gmail.com>
2026-07-11 23:03:28 +0700

    #

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-between gap-6">
         <div className="flex items-center gap-2">
           <img
-            src="/logoblack.svg"
+            src="/logocyan.svg"
             alt="Logo"
             className="h-10 w-auto"
           />
